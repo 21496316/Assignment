e29d03f43449a221f7be7ec0e16ecc5980968845
21496316 <twinkleharsora7@gmail.com>
2022-02-06 04:32:34 +0000

    4 files modified, 4 added, 4 deleted

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Menulist } from './Menulist';
+import './Navbar.css';
 const Navbar = () => {
-  const menulist = Menulist.map(({url,title},index) => {
-    return(
+  const menulist = Menulist.map(({ url, title }, index) => {
+    return (
       <li key={index}>
         <a href={url}>{title}</a>
       </li>
-    )
+    );
   });
-  return(
+  return (
     <nav>
-      <div Classname='logo'>
+      <div classname="logo">
         VPN <font>Lab</font>
       </div>
       <ul className="menu-list">{menulist}</ul>
